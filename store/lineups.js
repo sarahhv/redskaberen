d4e96b9e49e7d227ff6f7ead256ad958518b101d
Sarah <eaaskh@students.eaaa.dk>
2020-11-11 12:05:30 +0100

    Two lineups to lineupstore

--- store/lineups.js
+++ store/lineups.js
@@ -3,12 +3,12 @@ export const state = () => ({
     {
       id: 0,
       programId: 4,
-      img: '/_nuxt/assets/img/lineupOne.png',
+      img: 'one',
     },
     {
       id: 1,
       programId: 5,
-      img: 'https://docplayer.dk/docs-images/93/112665108/images/22-0.jpg',
+      img: 'two',
     },
   ],
 });
