63c9fe98a2247bec6989a8e53590836ee2120e0b
Sarah <eaaskh@students.eaaa.dk>
2020-11-13 11:53:07 +0100

    Lineup naming

--- components/Lineup.vue
+++ components/Lineup.vue
@@ -4,13 +4,13 @@
     <!-- Div containing the loop over the programs -->
     <div v-for="pl in programLineup" :key="pl.id">
       <!-- Div containing the if statement - stating to only show the program id matching the clicked team  -->
-      <div v-if="$route.params.id == pl.id" class="lineup-container__lineup">
-        <h1 class="lineup-container__lineup__title">Redskabsopstilling for</h1>
+      <div v-if="$route.params.id == pl.id" class="lineups">
+        <h1 class="lineups__title">Redskabsopstilling for</h1>
         <!-- The teamname of the clicked team from the program -->
-        <p class="lineup-container__lineup__program-details">{{ pl.team }}</p>
-        <p class="lineup-container__lineup__program-details">{{ pl.time }}</p>
+        <p class="lineups__program-details">{{ pl.team }}</p>
+        <p class="lineups__program-details">{{ pl.time }}</p>
         <!-- Image of the lineup -->
-        <img :src="require(`../assets/img/${pl.lineupImg}.png`)" :alt="'Lineup' + pl.team" class="lineup-container__lineup__img" />
+        <img :src="require(`../assets/img/${pl.lineupImg}.png`)" :alt="'Lineup' + pl.team" class="lineups__img" />
       </div>
     </div>
   </section>
@@ -35,20 +35,20 @@ export default {
     color: black;
     text-decoration: none;
   }
-  &__lineup {
-    height: 96vh;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    &__program-details {
-      background: #ffffffcc;
-      display: inline-flex;
-      margin: 0 auto;
-    }
-    &__img {
-      width: 100%;
-      margin: auto;
-    }
+}
+.lineups {
+  height: 96vh;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  &__program-details {
+    background: #ffffffcc;
+    display: inline-flex;
+    margin: 0 auto;
+  }
+  &__img {
+    width: 100%;
+    margin: auto;
   }
 }
 </style>
