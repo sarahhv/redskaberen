2319a1c2ef2bbf93e4ff5047a7fd2eb32c86a7c1
Sarah <eaaskh@students.eaaa.dk>
2020-11-13 11:54:14 +0100

    Date in the p tag
    Also table thead and tbody

--- components/Program.vue
+++ components/Program.vue
@@ -2,31 +2,43 @@
   <section class="program-container">
     <!-- Should be fetched data both h1 and p tag -->
     <h1>Børnedag i Vejlby-Risskov Hallen, Aarhus</h1>
-    <p>Lørdag den 09 marts 2019</p>
+    <p>Lørdag den 09 marts 2019. Hello! {{ todayDay }} den {{ todayDate }}</p>
     <!-- Table for the show-program that contains a row with headers and a row with a loop of the programData -->
     <table class="program">
-      <tr class="program__headers">
-        <th class="program__headers__header time">Tid</th>
-        <th class="program__headers__header team">Hold</th>
-        <th class="program__headers__header instructors">Instruktører</th>
-      </tr>
-      <tr v-for="plan in program" :key="plan.id" class="program__infos" :class="{ announcement: plan.announcement }">
-        <!-- Show time - dansk: Opvisningstiden -->
-        <td class="program__infos__info time">{{ plan.time }}</td>
-        <!-- Show team - dansk: Opvisningsholdet -->
-        <td v-if="plan.lineup" class="program__infos__info team">
-          <nuxt-link :to="'/lineups/' + plan.id" class="program__infos__info__links">{{ plan.team }}</nuxt-link>
-        </td>
-        <td v-else class="program__infos__info team">{{ plan.team }}</td>
-        <!-- Show team instructors - dansk: Opvisningsholdets instruktører -->
-        <td class="program__infos__info instructors">{{ plan.instructors }}</td>
-      </tr>
+      <thead>
+        <tr class="program__headers">
+          <th class="program__headers__header time">Tid</th>
+          <th class="program__headers__header team">Hold</th>
+          <th class="program__headers__header instructors">Instruktører</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="plan in program" :key="plan.id" class="program__infos" :class="{ announcement: plan.announcement }">
+          <!-- Show time - dansk: Opvisningstiden -->
+          <td class="program__infos__info time">{{ plan.time }}</td>
+          <!-- Show team - dansk: Opvisningsholdet -->
+          <td v-if="plan.lineup" class="program__infos__info team">
+            <nuxt-link :to="'/lineups/' + plan.id" class="program__infos__info__links">{{ plan.team }}</nuxt-link>
+          </td>
+          <td v-else class="program__infos__info team">{{ plan.team }}</td>
+          <!-- Show team instructors - dansk: Opvisningsholdets instruktører -->
+          <td class="program__infos__info instructors">{{ plan.instructors }}</td>
+        </tr>
+      </tbody>
     </table>
   </section>
 </template>
 
 <script>
+var dayjs = require('dayjs');
+var today = dayjs(new Date());
 export default {
+  data() {
+    return {
+      todayDay: today.format('dddd'),
+      todayDate: today.format('D MMMM YYYY'),
+    };
+  },
   computed: {
     program() {
       return this.$store.state.program.program;
@@ -59,7 +71,7 @@ p {
   &__infos {
     background-color: var(--color-pixie-green);
     font-weight: var(--base-font-light-weight);
-    &:nth-child(odd) {
+    &:nth-child(even) {
       background-color: var(--color-willow-brook);
     }
     &__info {
